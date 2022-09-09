98341fb6d45cf02bf52fb7cff3be28879fe0aae7
AracelyC1 <aracely.castro@usach.cl>
2022-09-08 23:31:26 -0300

    avance del fliH, puede mejorar. Agrege selectores para recorrer mejor elementos de un pixel

--- TDA_Image.rkt
+++ TDA_Image.rkt
@@ -68,6 +68,20 @@
 (define image_3 (image 2 2 pixhex_1 pixhex_2 pixhex_3 pixhex_4))
 
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; SELECTORES ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;,
+
+
+; funciones selectores para recorrer las 6 posiciones de un pixel
+;
+(define pos_0 (lambda (L) (list-ref L 0))) ; x
+(define pos_1 (lambda (L) (list-ref L 1))) ; y
+(define pos_2 (lambda (L) (list-ref L 2))) ; bit, hex, c1
+(define pos_3 (lambda (L) (list-ref L 3))) ; c2, bit
+(define pos_4 (lambda (L) (list-ref L 4))) ; c3, d
+(define pos_5 (lambda (L) (list-ref L 5))) ; profundidad d
+
+
 ;----------------------------- PERTENENCIA --------------------------------------------------------------------
 ; definir pixmap-check
 ; Dom: x (int) X y (int) X c1 (int) X c2 (int) X c3 (int)
@@ -133,7 +147,78 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+; fliH
+(define largo_pos_x (lambda (image) (- (width_image image) 1)))
+(define largo_pos_y (lambda (image) (- (height_image image) 1)))
 
+(define modificar_posicion_pixel (lambda (pos_x pixel)
+     (cond
+       [(bitmap-check pixel) (pixbit-d (pos_0 pixel) pos_x (pos_2 pixel) (pos_3 pixel))]
+       [(hexmap-check pixel) (pixhex-d (pos_0 pixel) pos_x (pos_2 pixel) (pos_3 pixel))]
+       [(pixmap-check pixel) (pixrgb-d (pos_0 pixel) pos_x (pos_2 pixel) (pos_3 pixel) (pos_4 pixel) (pos_5 pixel))]
+       
+       )))
+
+
+(define pos_x_final? (lambda (pos_x pos_y image)
+    (if (< pos_y (largo_pos_y image))
+        (largo_pos_x image)
+        pos_x
+        )
+
+                       ))
+
+; funcion que te entrega una lista de las posiciones invertidas pixeles
+(define flipH-formato (lambda (formato_fila image)
+
+     (define flipHN (lambda (formato_fila pos_x)
+
+         (cond
+         [(null? formato_fila) null]
+         [(< pos_x 0) null]
+         [else (reverse (cons (modificar_posicion_pixel pos_x (car formato_fila)) (flipHN (cdr formato_fila) (- pos_x 1))))
+               ]) 
+
+          ))
+
+      (flipHN formato_fila (largo_pos_x image))
+                ))
+
+(define filtro_nulos (lambda (lista)
+    (if (null? lista)
+        null
+        (if (null? (car lista))
+            null
+            (cons (car lista) (filtro_nulos (cdr lista)))))
+                       ))
+
+(define fila_n (lambda (n lista)
+    (if (null? lista)
+        null
+        (if (= (car(car lista)) n)
+            (cons (car lista) (fila_n n (cdr lista)))
+            (fila_n n (cdr lista))
+            ))))
+
+(define flipH (lambda (image)
+
+    (define flipHC (lambda (pos_y)
+         (if (>= (height_image image) 2)      
+             (if (= pos_y (largo_pos_y image))
+                 null
+                 (append (flipH-formato (fila_n pos_y (format_image image)) image) (flipHC (+ pos_y 1)))
+                 )
+             (flipH-formato (fila_n pos_y (format_image image)) image)
+            )))
+         
+    (flipHC 0)
+                ))
+
+(define lista (format_image image_1))
+
+(define lista_n (fila_n 0 lista))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 
